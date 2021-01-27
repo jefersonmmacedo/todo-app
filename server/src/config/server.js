4ac0e6991ad302993c7aec9519947eae5db24b3e
Jeferson Macedo <jefersonmacedowgf@gmail.com>
2021-01-27 17:53:39 -0300

    Back-end finalizado

--- server/src/config/server.js
+++ server/src/config/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const Cors = require('./cors')
 
 app.unsubscribe(bodyParser.urlencoded({extended: true}))
 app.unsubscribe(bodyParser.json())
+app.use(Cors)
 
 app.get('/', (req, res) => {
     res.send('Serviro Iniciado com sucesso. Show!')
